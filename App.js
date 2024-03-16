a7a9930a1632fe032961cf4320e4eeb4a3cb76dc
LSTu5 <144841130+LSTu5@users.noreply.github.com>
2024-03-16 16:50:47 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+// App.js
+
+import React from 'react';
+import Navbar from './Navbar'; // Import the Navbar component
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar /> {/* Render the Navbar component */}
+      {/* Your other content goes here */}
+        <h1>Contact<h2>Reach out to us at</h2><h2>noemptyseats@gmail.com</h2></h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Navbar.js
@@ -0,0 +1,18 @@
+// Navbar.js
+
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <ul className="nav-links">
+                <li><a href="/">Home</a></li>
+                <li><a href="/co">Collaborations</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
